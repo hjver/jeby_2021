8b919344a4edf59628d24730bada5e9c083569f9
khj67515 <khj67515@naver.com>
2021-12-07 14:16:55 +0900

    로그아웃 기능 개발 완료

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from news import views as view_news
 
 urlpatterns = [
+    path("", view_news.index),
     path("admin/", admin.site.urls),
     path("common/", include("common.urls")),
+    path("news/", include("news.urls")),
 ]
